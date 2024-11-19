28bd490edcde1405271d784d8ad32a44b75607b7
saoirseburke <saoirseburke2@gmail.com>
2024-11-19 20:34:01 +0000

    update

--- generate_base64.py
+++ generate_base64.py
@@ -1,7 +1,7 @@
 import base64
 
 # Open the image and encode it to base64 -test
-with open("assests/profilepic.png", "rb") as image_file:
+with open("assets/profilepic.png", "rb") as image_file:
     encoded_image = base64.b64encode(image_file.read()).decode()
 
 # Print the base64-encoded image string
