a007197d70273a508fcd2998d0af3234e9f39ed5
saoirseburke <saoirseburke2@gmail.com>
2024-11-19 20:01:36 +0000

    test

--- generate_base64.py
+++ generate_base64.py
@@ -1,6 +1,6 @@
 import base64
 
-# Open the image and encode it to base64
+# Open the image and encode it to base64 -test
 with open("assests/profilepic.png", "rb") as image_file:
     encoded_image = base64.b64encode(image_file.read()).decode()
 
